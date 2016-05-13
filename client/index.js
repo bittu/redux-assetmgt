5471b1b67c715b12d56cff7a346887cff891e3be
Sandeep Vemula <bittu4u.4ever@gmail.com>
2016-05-13 19:32:14 +0530

    update

---
+++ client/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { hashHistory } from 'react-router';
+
+import Root from './containers/Root';
+import configureStore from './store/configureStore';
+import { loginUserSuccess } from './actions';
+
+const target = document.getElementById('root');
+const { store, history } = configureStore(hashHistory, window.__INITIAL_STATE__);
+
+const node = (
+	<Root store={store} history={history} />
+	);
+
+let token = localstorage.getItem('token');
+
+if(token !== null) {
+	store.dispatch(loginUserSuccess(token));
+}
+
+ReactDOM.render(node, target);
